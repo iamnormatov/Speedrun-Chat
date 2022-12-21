7566d293f23a612005885fd2bd3c67f82c7e2137
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-22 00:18:04 +0500

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'azizjon@gmail.com',
+            password: 'password',
+            password_confirmation: 'password')
+
+User.create(email: 'normatov@gmail.com',
+            password: 'password',
+            password_confirmation: 'password')
+
+Room.create!(name: 'General')
+Room.create!(name: 'Testintg')
+Room.create!(name: 'Random')
